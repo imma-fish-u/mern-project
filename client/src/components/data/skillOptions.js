6f811ec653e0925a897e08b015d6ec0e22da014b
imma-fish-u <stankoagregatBel@gmail.com>
2022-10-02 18:10:51 +0300

    fix: skillOPtions problem

--- client/src/components/data/skillOptions.js
+++ client/src/components/data/skillOptions.js
@@ -1,15 +1,26 @@
-export const skillCategories = ["Software Developer", "Designer"];
+export const skillCategories = [
+  "All",
+  "Front-end",
+  "Back-end",
+  "Designer",
+  "BA Analytics",
+  "Game Dev",
+];
 
 export const skillOptions = [
-    { value: "JavaScript", label: "JavaScript", category: "Software Developer" },
-    { value: "React", label: "React", category: "Software Developer" },
-    { value: "NextJs", label: "NextJs", category: "Software Developer" },
-    { value: "PHP", label: "PHP", category: "Software Developer" },
-    { value: "Node", label: "Node", category: "Software Developer" },
-    { value: "Python", label: "Python", category: "Software Developer" },
-    { value: "Java", label: "Java", category: "Software Developer" },
-    { value: "C++", label: "C++", category: "Software Developer" },
-    { value: "Adobe Illustrator", label: "Adobe Illustrator", category: "Designer" },
-    { value: "Adobe Photoshop", label: "Adobe Photoshop", category: "Designer" },
-    { value: "Figma", label: "Figma", category: "Designer" },
+  { value: "JavaScript", label: "JavaScript", category: "Front-end" },
+  { value: "React", label: "React", category: "Front-end" },
+  { value: "NextJs", label: "NextJs", category: "Front-end" },
+  { value: "PHP", label: "PHP", category: "Back-end" },
+  { value: "Node", label: "Node", category: "Back-end" },
+  { value: "Python", label: "Python", category: "Back-end" },
+  { value: "Java", label: "Java", category: "Back-end" },
+  { value: "C++", label: "C++", category: "Game dev" },
+  {
+    value: "Adobe Illustrator",
+    label: "Adobe Illustrator",
+    category: "Designer",
+  },
+  { value: "Adobe Photoshop", label: "Adobe Photoshop", category: "Designer" },
+  { value: "Figma", label: "Figma", category: "Designer" },
 ];
--- client/src/pages/Resumes.js
+++ client/src/pages/Resumes.js
@@ -1,73 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { getResumes } from '../redux/actions/resume.actions';
+import { connect, useDispatch, useSelector } from "react-redux";
+import { getResumes } from "../redux/actions/resume.actions";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import PageTemplate from '../components/templates/PageTemplate';
-import Loader from '../components/templates/resume/Loader';
-import Card from '../components/templates/resume/Card';
-import Search from '../components/utils/Search';
+import PageTemplate from "../components/templates/PageTemplate";
+import Loader from "../components/templates/resume/Loader";
+import Card from "../components/templates/resume/Card";
+import Search from "../components/utils/Search";
 
 import { skillCategories, skillOptions } from "../components/data/skillOptions";
 
 const Resumes = () => {
-    const [categoryFilter, setCategoryFilter] = useState('');
-    const [skillFilter, setSkillFilter] = useState([]);
-    const resumes = useSelector((state) => state.resumeReducer.resumes);
-    const dispatch = useDispatch();
-    const loading = false;
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [skillFilter, setSkillFilter] = useState([]);
+  const resumes = useSelector((state) => state.resumeReducer.resumes);
+  const dispatch = useDispatch();
+  const loading = false;
 
-    useEffect(() => {
-        dispatch(getResumes());
-    }, []);
+  useEffect(() => {
+    dispatch(getResumes());
+  }, []);
 
-    if (loading) {
-        return <Loader />;
-    }
+  if (loading) {
+    return <Loader />;
+  }
 
-    const handleFilter = (value) => {
-        setSkillFilter([...skillFilter, value]);
-    };
+  const handleFilter = (value) => {
+    setSkillFilter([...skillFilter, value]);
+  };
 
-    return (
-        <PageTemplate pageTitle="Allresumes">
-            <div className="wrapper allboards">
-                <div className="allresumes__top">
-                    <h1 className="allresumes__top__title">Все резюме</h1>
-                    <Search />
-                </div>
+  return (
+    <PageTemplate pageTitle="Allresumes">
+      <div className="wrapper allboards">
+        <div className="allresumes__top">
+          <h1 className="allresumes__top__title">Все резюме</h1>
+          <Search />
+        </div>
 
-                <div className="allresumes__category-filter">
-                    {skillCategories.map((category) => 
-                        <div onClick={() => setCategoryFilter(category)}>{category}</div>
-                    )}
-                </div>
+        <div className="allresumes__category-filter">
+          {skillCategories.map((category) => (
+            <div onClick={() => setCategoryFilter(category)}>{category}</div>
+          ))}
+        </div>
 
-                <div className="allresumes__skill-filter">
-                    <ul className="allresumes__container__items__labels">
-                        { skillOptions.include(categoryFilter).map(({ value }, el) => (
-                            <li key={el} onClick={() => handleFilter(value) } className="cardlabel allresumes__container__items__labels__skill">{ value }</li>
-                        ))}
-                    </ul>
-                </div>
-                
-                <div className='allresumes__container'>
-                    {!loading && resumes.length === 0 ? (
-                        <p className='center'>Пока что нет резюме...</p>
-                    ) : (
-                            resumes.map((resume) => (
-                                <Link to={`/profile/view/:${resume.owner}`} key={resume._id} className="allresumes__container__items">
-                                    <Card { ...resume }></Card>
-                                </Link>
-                            )
-                        )
-                    )}
-                </div>
-            </div>
-        </PageTemplate>
-    );
+        <div className="allresumes__skill-filter">
+          <ul className="allresumes__container__items__labels">
+            {categoryFilter !== "All"
+              ? skillOptions
+                  .filter((item) => item.category === categoryFilter)
+                  .map(({ value }, el) => (
+                    <li
+                      key={el}
+                      onClick={() => handleFilter(value)}
+                      className="cardlabel allresumes__container__items__labels__skill"
+                    >
+                      {value}
+                    </li>
+                  ))
+              : skillOptions.map(({ value }, el) => (
+                  <li
+                    key={el}
+                    onClick={() => handleFilter(value)}
+                    className="cardlabel allresumes__container__items__labels__skill"
+                  >
+                    {value}
+                  </li>
+                ))}
+          </ul>
+        </div>
+
+        <div className="allresumes__container">
+          {!loading && resumes.length === 0 ? (
+            <p className="center">Пока что нет резюме...</p>
+          ) : (
+            resumes.map((resume) => (
+              <Link
+                to={`/profile/view/:${resume.owner}`}
+                key={resume._id}
+                className="allresumes__container__items"
+              >
+                <Card {...resume}></Card>
+              </Link>
+            ))
+          )}
+        </div>
+      </div>
+    </PageTemplate>
+  );
 };
 
 export default connect()(Resumes);
